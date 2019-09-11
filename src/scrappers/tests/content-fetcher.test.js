3ea8b2d62561a5e6b85f8abf37cabfa62203ee0b
Suraj Shrestha <syuraj@gmail.com>
2019-09-11 01:45:41 -0400

    Use html-to-text library to get text from html

--- src/scrappers/tests/content-fetcher.test.js
+++ src/scrappers/tests/content-fetcher.test.js
@@ -3,11 +3,19 @@ import ContentFetcher from '../content-fetcher'
 jest.setTimeout(20000)
 
 describe('content-fetcher', () => {
-	it(' should fetch articles successfully', async () => {
+	it('fetchArticles should fetch articles successfully', async () => {
 		const { articles } = await ContentFetcher.fetchArticles()
 
 		console.log('Printing articles', articles)
 
 		expect(articles.length).toBeGreaterThan(5)
+	})
+
+	it('scrapeArticleLink should scrape a given article link', async () => {
+		const article = await ContentFetcher.scrapeArticleLink(
+			'https://medium.com/siristechnology/how-to-run-node-express-backend-for-gatsby-app-87ee63b14ee9'
+		)
+
+		expect(article.contentText.length).toBeGreaterThan(10)
 	})
 })
