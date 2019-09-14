6afc5b74480d87017d2dca7ed4b4d0f7e5bd234f
Suraj Shrestha <syuraj@gmail.com>
2019-09-13 23:22:44 -0400

    Revert "Fix netlify graphql (#7)"
    This reverts commit 6fc9599119f8106723d5d11333f156f0ca89f24c.

--- src/functions/graphql.js
+++ src/functions/graphql.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 import { ApolloServer } from 'apollo-server-lambda'
 import { resolver } from '../graphql/resolvers'
-import typeDefs from '../graphql/typeDefs.graphql'
+import fs from 'fs'
+
+const typeDefs = fs.readFileSync('./src/graphql/typeDefs.graphql').toString('utf8')
 
 const server = new ApolloServer({
 	typeDefs: typeDefs,
