bdbf127c78e565a32c6e03f6c94c0486afba73d8
Suraj Shrestha <syuraj@gmail.com>
2019-09-13 21:18:02 -0400

    minor refactor

--- src/functions/graphql.js
+++ src/functions/graphql.js
@@ -3,8 +3,6 @@ import { ApolloServer } from 'apollo-server-lambda'
 import { resolver } from '../graphql/resolvers'
 import typeDefs from '../graphql/typeDefs.graphql'
 
-// const typeDefs = fs.readFileSync('./src/graphql/typeDefs.graphql').toString('utf8')
-
 const server = new ApolloServer({
 	typeDefs: typeDefs,
 	resolvers: resolver,
