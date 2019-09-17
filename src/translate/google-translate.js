e37dbbabbc1699b2f8763f771a748bac5afc49f9
Suraj Shrestha <syuraj@gmail.com>
2019-09-17 17:53:24 -0400

    Using new translate library to fix line breaks

--- src/translate/google-translate.js
+++ src/translate/google-translate.js
@@ -1,29 +1,36 @@
 require('dotenv').config()
-var GoogleTranslate = require('google-translate')(process.env.GOOGLE_TRANSLATE_API_KEY)
+const { Translate } = require('@google-cloud/translate')
 
 module.exports = {
 	translateArticle: async function(content, language) {
-		const strippedContent = content.replace(/\[[^\]]*\]/g, '')
+		const strippedContent = content.replace(/\[[^\]]*\]/g, '').slice(0, 5000)
 		const translatedText = await this.googleTranslate(strippedContent, language)
 
 		const result = {
-			originalContent: strippedContent.split(/[.\n]+/),
-			translatedContent: translatedText.split(/[.\n*]+/)
+			originalContent: this.breakLongTextIntoSentences(strippedContent),
+			translatedContent: this.breakLongTextIntoSentences(translatedText)
 		}
 		return result
 	},
 
 	googleTranslate: async function(content, language) {
 		return new Promise((resolve, reject) => {
-			GoogleTranslate.translate(content.slice(0, 5000), language, function(err, translation) {
+			const translator = new Translate({ key: process.env.GOOGLE_TRANSLATE_API_KEY })
+
+			translator.translate(content.slice(0, 5000), language, function(err, translation) {
 				if (!translation) {
+					console.log('Printing err', err)
 					console.log('Printing article', content)
 					console.log('Printing translation', translation)
 					reject('Translation failed')
 				} else {
-					resolve(translation.translatedText)
+					resolve(translation)
 				}
 			})
 		})
+	},
+
+	breakLongTextIntoSentences: function(longText) {
+		return longText.split(/[\n]+/)
 	}
 }
