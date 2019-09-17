9a17e3670bd82f613830cdf0663449944015a35c
Suraj Shrestha <syuraj@gmail.com>
2019-09-17 16:22:22 -0400

    WIP - split translation and originalcontent

--- src/translate/google-translate.js
+++ src/translate/google-translate.js
@@ -2,16 +2,22 @@ require('dotenv').config()
 var GoogleTranslate = require('google-translate')(process.env.GOOGLE_TRANSLATE_API_KEY)
 
 module.exports = {
-	translateArticle: async function(string, language) {
-		const translatedText = await this.googleTranslate(string, language)
-		return string + ' \n' + translatedText
+	translateArticle: async function(content, language) {
+		const strippedContent = content.replace(/\[[^\]]*\]/g, '')
+		const translatedText = await this.googleTranslate(strippedContent, language)
+
+		const result = {
+			originalContent: strippedContent.split(/[.\n]+/),
+			translatedContent: translatedText.split(/[.\n*]+/)
+		}
+		return result
 	},
 
-	googleTranslate: async function(string, language) {
+	googleTranslate: async function(content, language) {
 		return new Promise((resolve, reject) => {
-			GoogleTranslate.translate(string.slice(0, 5000), language, function(err, translation) {
+			GoogleTranslate.translate(content.slice(0, 5000), language, function(err, translation) {
 				if (!translation) {
-					console.log('Printing article', string)
+					console.log('Printing article', content)
 					console.log('Printing translation', translation)
 					reject('Translation failed')
 				} else {
