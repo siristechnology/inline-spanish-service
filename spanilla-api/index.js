70d68ef7944a05e3cfae51da2e0db248b0bc405a
Suraj Shrestha <syuraj@gmail.com>
2019-10-08 21:11:00 -0400

    use require-graphql-file to import graphql file

--- spanilla-api/index.js
+++ spanilla-api/index.js
@@ -1,12 +1,13 @@
 const { ApolloServer, gql } = require('apollo-server-azure-functions')
+const requireGraphQLFile = require('require-graphql-file')
 const resolvers = require('../src/graphql/resolvers.js')
-const fs = require('fs')
 
 process.env.NODE_ENV = 'development'
 
 console.log('Printing __dirname', __dirname)
 
-const typeDefSchema = fs.readFileSync('./src/graphql/typeDefs.graphql', 'utf8')
+const typeDefSchema = requireGraphQLFile('../src/graphql/typeDefs.graphql')
+
 const typeDefs = gql(typeDefSchema)
 
 const server = new ApolloServer({
