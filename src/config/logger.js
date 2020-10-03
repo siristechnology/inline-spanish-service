864f62a4ce9156dd424d983946f72366c36cfee5
Suraj Shrestha <syuraj@gmail.com>
2020-10-03 19:34:49 -0400

    refactor winston logger

--- src/config/logger.js
+++ src/config/logger.js
@@ -2,11 +2,17 @@ const winston = require('winston')
 require('dotenv').config()
 
 const logger = winston.createLogger({
-	transports: [new winston.transports.Console({ handleExceptions: true, level: process.env.LOG_LEVEL })],
+	transports: [
+		new winston.transports.Console({ handleExceptions: true, handleRejections: true, level: process.env.LOG_LEVEL })
+	],
+	exitOnError: false
 })
 
 logger.on('error', (error, transport) => {
-	console.log('Winston Logger crashed')
+	try {
+		// eslint-disable-next-line no-console
+		console.log('Winston Logger crashed', error)
+	} catch {}
 })
 
 module.exports = logger
