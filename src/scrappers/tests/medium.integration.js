1196e2cfa3d1bbb105bc0221d299ccee3d904d26
Suraj Shrestha <syuraj@gmail.com>
2021-03-07 15:31:39 -0500

    medium selector fix

--- src/scrappers/tests/medium.integration.js
+++ src/scrappers/tests/medium.integration.js
@@ -4,14 +4,16 @@ const SourceConfig = require('../config/source-configs.json')
 jest.setTimeout(60000)
 
 describe('content-fetcher', () => {
-	it('BBC articles should be fetched successfully', async () => {
-		const source = SourceConfig.filter((s) => s.sourceName === 'bbc')
+	it('Medium articles should be fetched successfully', async () => {
+		const source = SourceConfig.filter((s) => s.sourceName === 'medium')
 
 		const articles = await NewsCrawler(source, {
 			maxArticlesPerPage: 2,
 			articleUrlLength: 3,
 			headless: true
 		})
+
+		console.log('printing articles', articles)
 
 		expect(articles.length).toBeGreaterThan(0)
 	})
